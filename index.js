b0e30743773d8d7e4284b028c7260be9345c0f90
johney <johneyburke@gmail.com>
2023-09-27 20:37:04 -0600

    filter

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+/*function superbowlWin(gameRecords) {
+
+    if (gameRecords.result === "W"){
+         return gameRecords.year
+    }
+    else {
+  return undefined;
+}
+
+    
+}
+
+record.find(superbowlWin);
+*/
+function superbowlWin(gameRecords) {
+  const winningGame = gameRecords.find(gameRecord => gameRecord.result === "W");
+
+  return winningGame ? winningGame.year : undefined;
+}
+
+    
